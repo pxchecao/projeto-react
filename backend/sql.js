d56cde407a384f03cc92512fc8dcc9d8ade3d904
pxchecao <pepe1332016@gmail.com>
2025-10-16 12:23:47 -0300

    Delete backend directory

--- backend/sql.js
+++
@@ -1,7 +0,0 @@
-import postgres from "postgres";                                                          
-import dotenv from "dotenv"                                                               
-dotenv.config();
-
-export const sql = postgres(process.env.DATABASE_URL, {
-    ssl: 'require'
-});
